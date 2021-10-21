bec6f9ba55685955bee43c6d55906d0ba1aba887
YS <yoonsam86@gmail.com>
2021-10-22 00:17:36 +0900

    ResourceFetcher 추가

--- coil-base/src/main/java/com/ys/coil/util/Contexts.kt
+++ coil-base/src/main/java/com/ys/coil/util/Contexts.kt
@@ -2,7 +2,27 @@ package com.ys.coil.util
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.drawable.Drawable
+import androidx.annotation.DrawableRes
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.ContextCompat
+
+internal val HAS_APPCOMPAT_RESOURCES = try {
+    Class.forName(AppCompatResources::class.java.name)
+    true
+} catch (ignored: Throwable) {
+    false
+}
+
+internal fun Context.getDrawableCompat(@DrawableRes resId: Int): Drawable {
+    val drawable = if (HAS_APPCOMPAT_RESOURCES) {
+        AppCompatResources.getDrawable(this, resId)
+    } else {
+        ContextCompat.getDrawable(this, resId)
+    }
+
+    return checkNotNull(drawable)
+}
 
 internal inline fun Context.isPermissionGranted(permission: String): Boolean {
     return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
--- coil-base/src/main/java/com/ys/coil/util/Extensions.kt
+++ coil-base/src/main/java/com/ys/coil/util/Extensions.kt
@@ -3,6 +3,13 @@ package com.ys.coil.util
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import androidx.core.graphics.drawable.toDrawable
 
 internal inline fun Bitmap.toDrawable(context: Context): BitmapDrawable = toDrawable(context.resources)
+
+internal val Drawable.width: Int
+    get() = (this as? BitmapDrawable)?.bitmap?.width ?: intrinsicWidth
+
+internal val Drawable.height: Int
+    get() = (this as? BitmapDrawable)?.bitmap?.height ?: intrinsicHeight
