9b2f32e7630f4d5ad66fc373ac72993127c61a24
YS <yoonsam86@gmail.com>
2022-02-07 09:05:47 +0900

    coil-singleton Coil 싱글톤 추가

--- coil-singleton/src/main/java/com/ys/coil/Coil.kt
+++ coil-singleton/src/main/java/com/ys/coil/Coil.kt
@@ -1,7 +1,12 @@
 package com.ys.coil
 
 import android.content.Context
+import androidx.annotation.VisibleForTesting
 import com.ys.coil.Coil.imageLoader
+import com.ys.coil.Coil.setImageLoader
+import com.ys.coil.request.Disposable
+import com.ys.coil.request.ImageRequest
+import com.ys.coil.request.ImageResult
 
 /**
  * 싱글톤 [ImageLoader] 인스턴스를 보유하는 클래스.
@@ -20,6 +25,30 @@ object Coil {
 	@JvmStatic
 	fun imageLoader(context: Context) = imageLoader ?: newImageLoader(context)
 
+	/**
+	 * 싱글톤 [ImageLoader]를 설정합니다.
+	 * [ImageLoader]를 느리게 생성하려면 `setImageLoader(ImageLoaderFactory)`를 사용하는 것이 좋습니다.
+	 */
+	@JvmStatic
+	@Synchronized
+	fun setImageLoader(imageLoader: ImageLoader) {
+		this.imageLoaderFactory = null
+		this.imageLoader = imageLoader
+	}
+
+	/**
+	 * 싱글톤 [ImageLoader]를 생성하는 데 사용할 [ImageLoaderFactory]를 설정합니다.
+	 * [factory]는 최대 한 번만 호출되도록 보장됩니다.
+	 *
+	 * 참고: [Factory]는 [ImageLoaderFactory]를 구현하는 [Application]보다 우선합니다.
+	 */
+	@JvmStatic
+	@Synchronized
+	fun setImageLoader(factory: ImageLoaderFactory) {
+		imageLoaderFactory = factory
+		imageLoader = null
+	}
+
 	/** 새 싱글톤 [ImageLoader]를 만들고 설정합니다. */
 	@Synchronized
 	private fun newImageLoader(context: Context): ImageLoader {
@@ -35,4 +64,35 @@ object Coil {
 		imageLoader = newImageLoader
 		return newImageLoader
 	}
+
+	/** 내부 상태를 재설정합니다. */
+	@VisibleForTesting
+	@Synchronized
+	internal fun reset() {
+		imageLoader = null
+		imageLoaderFactory = null
+	}
+
+	@Deprecated(
+		message = "Replace with 'context.imageLoader.enqueue(request)'.",
+		replaceWith = ReplaceWith(
+			expression = "request.context.imageLoader.enqueue(request)",
+			imports = ["coil.imageLoader"]
+		),
+		level = DeprecationLevel.ERROR // Temporary migration aid.
+	)
+	@JvmStatic
+	fun enqueue(request: ImageRequest): Disposable = error("Unsupported")
+
+	@Deprecated(
+		message = "Replace with 'context.imageLoader.execute(request)'.",
+		replaceWith = ReplaceWith(
+			expression = "request.context.imageLoader.execute(request)",
+			imports = ["coil.imageLoader"]
+		),
+		level = DeprecationLevel.ERROR // Temporary migration aid.
+	)
+	@JvmStatic
+	@Suppress("RedundantSuspendModifier")
+	suspend fun execute(request: ImageRequest): ImageResult = error("Unsupported")
 }
