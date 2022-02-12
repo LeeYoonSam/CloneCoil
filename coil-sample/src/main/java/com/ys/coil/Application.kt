ed9fe40b0dd19bc84ec452f19ce66f530e305037
YS <yoonsam86@gmail.com>
2022-02-12 16:04:19 +0900

    coil-sample - Application 에 ImageLoader 생성하는 코드 추가

--- coil-sample/src/main/java/com/ys/coil/Application.kt
+++ coil-sample/src/main/java/com/ys/coil/Application.kt
@@ -1,6 +1,47 @@
 package com.ys.coil
 
 import android.app.Application
+import android.util.Log
+import com.ys.coil.disk.DiskCache
+import com.ys.coil.memory.MemoryCache
+import com.ys.coil.util.DebugLogger
+import okhttp3.Dispatcher
+import okhttp3.OkHttpClient
 
-class Application : Application() {
+class Application : Application(), ImageLoaderFactory {
+	override fun newImageLoader(): ImageLoader {
+		return ImageLoader.Builder(this)
+			.memoryCache {
+				MemoryCache.Builder(this)
+					// Set the max size to 25% of the app's available memory.
+					.maxSizePercent(0.25)
+					.build()
+			}
+			.diskCache {
+				DiskCache.Builder(this)
+					.directory(filesDir.resolve("image_cache"))
+					.maxSizeBytes(512L * 1024 * 1024) // 512MB
+					.build()
+			}
+			.okHttpClient {
+				// 호스트별로 동시 네트워크 요청을 제한하지 마십시오.
+				val dispatcher = Dispatcher().apply { maxRequestsPerHost = maxRequests }
+
+				// 네트워크 작업에 사용되는 OkHttpClient를 Lazily 생성합니다.
+				OkHttpClient.Builder()
+					.dispatcher(dispatcher)
+					.build()
+			}
+			// 이미지를 비동기식으로 로드할 때 짧은 크로스페이드를 표시합니다.
+			.crossfade(true)
+			// 네트워크 캐시 헤더를 무시하고 항상 디스크 캐시에서 읽고 쓰십시오.
+			.respectCacheHeaders(false)
+			.apply {
+				// 디버그 빌드인 경우 표준 Android 로그에 대한 로깅을 활성화합니다.
+				if (BuildConfig.DEBUG) {
+					logger(DebugLogger(Log.VERBOSE))
+				}
+			}
+			.build()
+	}
 }
